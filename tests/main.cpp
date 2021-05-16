21a3385c6e926bcaab8782706b9a8d5c0be85139
rohanbehera03 <rohanbehera03@gmail.com>
2021-05-16 11:46:18 -0700

    Update main.cpp and add factory.hpp file

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,30 +1,11 @@
-#include <iostream>
+ 
+#include "../header/factory.hpp"
 
-#include "base.hpp"
-#include "mult.hpp"
-#include "div.hpp"
-#include "sub.hpp"
-#include "add.hpp"
-#include "op.hpp"
-#include "rand.hpp"
-#include "pow.hpp"
-#include <sstream>
-#include <string>
+int main(int argv, char** argc){
 
-using namespace std;
+Factory* factory = new Factory();
+Base* calculate = factory -> parse(argc, argv);
+cout << calculate -> stringify() << endl << calculate -> evaluate() << endl;
 
-int main() {
-    // This is a very basic main, and being able to correctly execute this main
-    // does not constitute a completed lab. Make sure you write unit tests for
-    // all the classes that you create (and can be instantiated) in this lab
-    Base* three = new Op(3);
-    Base* seven = new Op(7);
-    Base* four = new Op(4);
-    Base* two = new Op(2);
-    Base* mult = new Mult(seven, four);
-    Base* add = new Add(three, mult);
-    Base* minus = new Sub(add, two);
-
-    std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
-    return 0;
+return 0;
 }
