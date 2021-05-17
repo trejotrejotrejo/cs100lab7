a9d96b7be59649e1da5a5ecfb6633ed89051e3fc
trejotrejotrejo <alextrejo10101@gmail.com>
2021-05-16 21:43:45 -0700

    Added tests

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -2,6 +2,224 @@
 
 #include "op_test.hpp"
 
+TEST(Factory, normalAdd) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "20";
+        test_val[2] = "+";
+        test_val[3] = "10";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(20.000000+10.000000)", calc->stringify());
+        EXPECT_EQ(30, calc->evaluate());
+}
+
+TEST(Factory, normalSub) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "29";
+        test_val[2] = "-";
+        test_val[3] = "14";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(29.000000-14.000000)", calc->stringify());
+        EXPECT_EQ(15, calc->evaluate());
+}
+
+TEST(Factory, negativeSub) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "6";
+        test_val[2] = "-";
+        test_val[3] = "16";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(6.000000-16.000000)", calc->stringify());
+        EXPECT_EQ(-10, calc->evaluate());
+}
+
+TEST(Factory, normalMult) {
+	Factory* factory = new Factory();
+
+	char* test_val[4]; 
+	test_val[0] = "./calculator"; 
+	test_val[1] = "5"; 
+	test_val[2] = "*";
+	test_val[3] = "2";
+
+	Base* calc = factory->parse(test_val, 4);
+
+	EXPECT_EQ("(5.000000*2.000000)", calc->stringify());
+	EXPECT_EQ(10, calc->evaluate());
+}
+
+TEST(Factory, zeroMult) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "0";
+        test_val[2] = "*";
+        test_val[3] = "2";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(0.000000*2.000000)", calc->stringify());
+        EXPECT_EQ(0, calc->evaluate());
+}
+
+TEST(Factory, normalDiv) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "15";
+        test_val[2] = "/";
+        test_val[3] = "3";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(15.000000/3.000000)", calc->stringify());
+        EXPECT_EQ(5, calc->evaluate());
+}
+
+TEST(Factory, zeroDiv) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "0";
+        test_val[2] = "/";
+        test_val[3] = "8";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(0.000000/8.000000)", calc->stringify());
+        EXPECT_EQ(0, calc->evaluate());
+}
+
+TEST(Factory, undefinedDiv) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "7";
+        test_val[2] = "/";
+        test_val[3] = "0";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(7.000000/0.000000)", calc->stringify());
+        EXPECT_EQ(-1, calc->evaluate());
+}
+
+TEST(Factory, normalPow) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "2";
+        test_val[2] = "**";
+        test_val[3] = "4";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(2.000000**4.000000)", calc->stringify());
+        EXPECT_EQ(16, calc->evaluate());
+}
+
+TEST(Factory, zeroPow) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "3";
+        test_val[2] = "**";
+        test_val[3] = "0";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(3.000000**0.000000)", calc->stringify());
+        EXPECT_EQ(1, calc->evaluate());
+}
+
+TEST(Factory, onePow) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "9";
+        test_val[2] = "**";
+        test_val[3] = "1";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ("(9.000000**1.000000)", calc->stringify());
+        EXPECT_EQ(9, calc->evaluate());
+}
+
+TEST(Factory, MultipleOperations) {
+        Factory* factory = new Factory();
+
+        char* test_val[12];
+        test_val[0] = "./calculator";
+        test_val[1] = "4";
+        test_val[2] = "*";
+        test_val[3] = "3";
+        test_val[4] = "+";
+        test_val[5] = "8";
+        test_val[6] = "/";
+        test_val[7] = "2";
+        test_val[8] = "**";
+        test_val[9] = "2";
+        test_val[10] = "-";
+        test_val[11] = "50";
+
+
+
+        Base* calc = factory->parse(test_val, 12);
+
+        EXPECT_EQ("(((((4.000000*3.000000)+8.000000)/2.000000)**2.000000)-50.000000)", calc->stringify());
+        EXPECT_EQ(50, calc->evaluate());
+}
+
+TEST(Factory, invalidInput) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "+";
+        test_val[2] = "**";
+        test_val[3] = "6";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ(nullptr, calc);
+}
+
+TEST(Factory, invalidNegativeInput) {
+        Factory* factory = new Factory();
+
+        char* test_val[4];
+        test_val[0] = "./calculator";
+        test_val[1] = "-2";
+        test_val[2] = "**";
+        test_val[3] = "3";
+
+        Base* calc = factory->parse(test_val, 4);
+
+        EXPECT_EQ(nullptr, calc);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
